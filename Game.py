90bcdd693fbeb8274946444ca9e7fe14bbd81296
Mario Vela <mv516@ic.ac.uk>
2017-10-18 13:28:37 +0100

    Keep track of played game cards separately from trick cards. Added comments and corrected style.

--- Game.py
+++ Game.py
@@ -3,6 +3,8 @@ from Trick import Trick
 
 
 class Game(object):
+    """Game object, plays a number of tricks and awards points depending
+    on the outcome of the tricks and the predictions."""
 
     def __init__(self, game_num, players):
         self.game_num = game_num
@@ -10,28 +12,42 @@ class Game(object):
         self.deck = Deck()
         self.predictions = [-1]*len(players)
         self.trump_card = None
-        self.first_player = game_num % len(players)
+        # -1 adjusts for 1-index in game numbers and 0-index in players
+        self.first_player = (game_num-1) % len(players)
         self.played_cards = []
 
     def play(self):
         print("Playing game #{}".format(self.game_num))
+        # New game, new deck. No played cards.
         self.played_cards = []
         self.trump_card = self.distribute_cards()[0]
         if self.trump_card is None:
+            # We distributed all cards, the trump is N. (No trump)
             self.trump_card = Card("White", 0)
         else:
             self.played_cards.append(self.trump_card)
         if self.trump_card.value == 14:
-            self.trump_card.color = self.players[self.first_player].get_trump_color()
+            # Trump card is a Z, ask the dealer for a trump color.
+            self.trump_card.color =\
+                self.players[self.first_player].get_trump_color()
+        # Now that each player has a hand, ask for predictions.
         self.ask_for_predictions()
         # print("Final predictions {}".format(self.predictions))
+        # Reset all wins.
         wins = [0]*len(self.players)
+        for i in range(len(self.players)):
+            self.players[i].wins = wins[i]
         for trick_num in range(self.game_num):
-            trick = Trick(self.trump_card, self.players, self.first_player, self.played_cards)
-            winner, played_cards = trick.play()
+            # Play a trick for each card in the hand (or game number).
+            trick = Trick(self.trump_card, self.players, self.first_player,
+                          self.played_cards)
+            winner, trick_cards = trick.play()
+            # Trick winner gets a win and starts the next trick.
             wins[winner] += 1
             self.first_player = winner
-            self.played_cards = played_cards
+            # Game keeps track of the played cards.
+            self.played_cards += trick_cards
+            # Update wins
             for i in range(len(self.players)):
                 self.players[i].wins = wins[i]
             print("{} won the game!".format(winner))
@@ -54,7 +70,9 @@ class Game(object):
             # Start with the first player and ascend, then reset at 0.
             current_player_index = (self.first_player + i) % num_players
             player = self.players[current_player_index]
-            prediction = player.get_prediction(self.trump_card, self.predictions)
+            prediction = player.get_prediction(self.trump_card,
+                                               self.predictions,
+                                               self.players)
             self.predictions[current_player_index] = prediction
             """print("Player {} predicted {}".format(current_player_index,
                                                   prediction))"""
--- Trick.py
+++ Trick.py
@@ -2,28 +2,32 @@ from Card import Card, is_new_winner
 
 
 class Trick(object):
-    def __init__(self, trump_card, players, first_player, played_cards):
+    def __init__(self, trump_card, players, first_player, played_cards_in_game):
         self.trump_card = trump_card
         self.players = players
         self.first_player = first_player
         self.first_card = None
-        self.played_cards = played_cards
+        self.played_cards_in_game = played_cards_in_game
 
     def play(self):
         winner = None
         num_players = len(self.players)
-        played_cards = []
+        trick_cards = []
         for i in range(num_players):
             player_index = (self.first_player+i) % num_players
             # Start with the first player and ascend, then reset at 0.
             player = self.players[player_index]
-            played_card = player.play_card(self.trump_card, self.first_card, self.played_cards)
-            played_cards.append(played_card)
+            played_card = player.play_card(self.trump_card, self.first_card,
+                                           trick_cards, self.players,
+                                           self.played_cards_in_game)
+            trick_cards.append(played_card)
             if self.first_card is None and played_card.value != 0:
                 self.first_card = played_card
-            if winner is None or is_new_winner(played_card, winner[0], self.trump_card, self.first_card):
+            if winner is None or is_new_winner(played_card, winner[0],
+                                               self.trump_card,
+                                               self.first_card):
                 winner = (played_card, player_index)
             """print("First card: {}\nTrump card: {}\nWinning: {}".format(self.first_card,
                                                                        self.trump_card,
                                                                        winner))"""
-        return winner[1], played_cards
+        return winner[1], trick_cards
